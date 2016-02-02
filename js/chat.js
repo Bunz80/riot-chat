a8a67b5a04a6dc884217558f17a792e9ace2433e
k-kuwahara <kuwahara@lepra.jp>
2016-02-02 13:03:33 +0900

    send and get messages

--- js/chat.js
+++ js/chat.js
@@ -1,13 +1,21 @@
-riot.tag2('app', '<h3>{opts.title}</h3> <form onsubmit="{send}"> <p>Please enter the text box</p> <input type="text" onkeyup="{input}"> <input __disabled="{!text}" type="submit" value="Send Message"> </form> <ul> <li each="{opts.items}"><label class="{key}">{value}</label></li> </ul>', '', '', function(opts) {
+riot.tag2('app', '<h3>{opts.title}</h3> <form onsubmit="{send}"> <p>Please enter the text box</p> <input type="text" onkeyup="{input}"> <input __disabled="{!text}" type="submit" value="Send Message"> </form> <ul> <li each="{items}"><label>{message}</label></li> </ul>', '', '', function(opts) {
       this.text = "";
+      this.disabled = true;
+      items = [];
+
+      opts.data_store.child('messages').on('child_added', function(data_snapshot) {
+         data_snapshot.forEach(function(obj) {
+            items.push({'message': obj.val()});
+         });
+         console.dir(this);
+      });
 
       this.input = function(e) {
          this.text = e.target.value;
       }.bind(this)
 
       this.send = function(e) {
-         opts.data_store.child('messages').push({
-            'message': e.target.value,
-         });
+         opts.data_store.child('messages').push({'messages': this.text});
+         this.text = '';
       }.bind(this)
 }, '{ }');
--- tag/chat.tag
+++ tag/chat.tag
@@ -6,21 +6,32 @@
       <input disabled={ !text } type="submit" value="Send Message">
    </form>
    <ul>
-      <li each="{ opts.items }"><label class="{ key }">{ value }</label></li>
+      <li each="{ items }"><label>{ message }</label></li>
    </ul>
 
 
    <script>
       this.text = "";
+      this.disabled = true;
+      items = [];
 
+      // get messages
+      opts.data_store.child('messages').on('child_added', function(data_snapshot) {
+         data_snapshot.forEach(function(obj) {
+            items.push({'message': obj.val()});
+         });
+         console.dir(this);
+      });
+
+      // input text-box check
       input(e) {
          this.text = e.target.value;
       }
 
+      // add message
       send(e) {
-         opts.data_store.child('messages').push({
-            'message': e.target.value,
-         });
+         opts.data_store.child('messages').push({'messages': this.text});
+         this.text = '';
       }
    </script>
 </app>
